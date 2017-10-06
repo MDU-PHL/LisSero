0f623f41853d5f02870b50bd68ac51ec4c4eae78
Anders Goncalves da Silva <andersgs@gmail.com>
2017-10-06 13:30:50 +1100

    Fixed indentation error

--- lissero/run_lissero.py
+++ lissero/run_lissero.py
@@ -40,7 +40,7 @@ def run_lissero(serotype_db, binarytype_db, debug, fasta):
         log_level = logging.DEBUG
     else:
         log_level = logging.INFO
-        logging.basicConfig(level=log_level)
+    logging.basicConfig(level=log_level)
     path_serodb = os.path.realpath(serotype_db)
     path_btdb = os.path.realpath(binarytype_db)
     sero_db = SerotypeDB(path_db=path_serodb,
