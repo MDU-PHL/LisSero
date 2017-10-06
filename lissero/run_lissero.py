96d9bef8ea9929cba5b77793fb19f8450d6c20d4
Anders Goncalves da Silva <andersgs@gmail.com>
2017-10-06 13:18:44 +1100

    Entry point now works

--- lissero/run_lissero.py
+++ lissero/run_lissero.py
@@ -13,26 +13,38 @@ from .Serotype import SerotypeDB
 
 @click.command()
 @click.option("-s", "--serotype_db", default=None,
-              envar='LISSERO_DB')
+              envvar='LISSERO_DB')
 @click.option("-b", "--binarytype_db", default=None,
-              envar='LISBT_DB')
+              envvar='LISBT_DB')
 @click.argument("fasta", nargs=-1)
 def run_lissero(serotype_db, binarytype_db, fasta):
     '''
-    In silico serogroup prediction for L.monocytogenes
+    In silico serogroup prediction for L. monocytogenes.
     Alleles: lmo1118, lmo0737, ORF2819, ORF2110, Prs
-    run_lissero FASTA [FASTA, [FASTA, [...]]]
 
-    Ref: Doumith et al. Differentiation of the major Listeria monocytogenes
+    In silico binary type prediction for L. monocytogenes.
+    Alleles: 1, 2, 4, 8, 16, 32, 64, 128
+
+    References:
+
+    * Doumith et al. Differentiation of the major Listeria monocytogenes
         serovars by multiplex PCR. J Clin Microbiol, 2004; 42:8; 3819-22
+
+    * Huang et al. Binary typing of *Listeria monocytogenes* isolates from
+        patients and food through multiplex PCR and reverse line hybridisation.
+        SA, Australia, 2007; 136–137.
+
     '''
     logging.basicConfig(level=logging.INFO)
     path_serodb = os.path.realpath(serotype_db)
     path_btdb = os.path.realpath(binarytype_db)
     sero_db = SerotypeDB(path_db=path_serodb,
                          db_type='serotype')
+    sero_db.check_db()
     bt_db = SerotypeDB(path_db=path_btdb,
-                       db_type='binarytype')
+                       db_type='binary_type',
+                       db_name='lisbt')
+    bt_db.check_db()
     blast = Blast()
     samples = Samples(fasta,
                       blast=blast,
