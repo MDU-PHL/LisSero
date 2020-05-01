269154a7a424c553d6b0203b137c84b21886030d
Anders Goncalves da Silva <andersgs@gmail.com>
2020-04-30 17:33:01 -0700

    Format to PEP8

--- lissero/scripts/Serotype.py
+++ lissero/scripts/Serotype.py
@@ -1,8 +1,8 @@
-'''
+"""
 A class to generate a Serotype
 
 All Serotyping logic stays here.
-'''
+"""
 
 import os
 import logging
@@ -17,27 +17,24 @@ import pkg_resources
 from .Blast import MakeBlastDB
 
 
-SEROTYPE_FASTA = 'db/sequences.fasta'
-#BINARYTYPE_FASTA = 'db/binary_sequences.fasta'
+SEROTYPE_FASTA = "db/sequences.fasta"
+# BINARYTYPE_FASTA = 'db/binary_sequences.fasta'
 
 
 class Typing:
 
-    BLAST_OUTFMT = '6'
-    OUTPUT_HEADER = ['ID', 'TYPE',
-                     'DB_VERSION']
+    BLAST_OUTFMT = "6"
+    OUTPUT_HEADER = ["ID", "TYPE", "DB_VERSION"]
 
-    def __init__(self, blast_run, db,
-                 cov=100,
-                 pid=100):
+    def __init__(self, blast_run, db, cov=100, pid=100):
         self.db_version = db.version
         path_db = os.path.realpath(db.db_name)
         self.blast = copy.deepcopy(blast_run)
         self.blast.add_db(path_db)
-        self.blast.add_option('-ungapped')
-        self.blast.add_option('-culling_limit', 1)
-        self.blast.add_option('-outfmt', self.BLAST_OUTFMT)
-        self.blast.add_option('-dust', 'no')
+        self.blast.add_option("-ungapped")
+        self.blast.add_option("-culling_limit", 1)
+        self.blast.add_option("-outfmt", self.BLAST_OUTFMT)
+        self.blast.add_option("-dust", "no")
         self.cov = cov
         self.pid = pid
 
@@ -45,7 +42,7 @@ class Typing:
         try:
             string = json.dumps(self.report, indent=4)
         except:
-            string = '{}'
+            string = "{}"
         return string
 
     def _blast_run(self):
@@ -54,20 +51,18 @@ class Typing:
     def _blast_parse(self):
         self.full_matches = set()
         self.partial_matches = set()
-        blast_matches = self.blast_res.stdout.strip().split('\n')
+        blast_matches = self.blast_res.stdout.strip().split("\n")
         for b in blast_matches:
-            (qaccver,
-             saccver,
-             length,
-             slen,
-             pident) = b.split('\t')
-            if float(pident) >= self.pid and\
-               100*(float(length)/float(slen)) >= self.cov:
-                self.full_matches.update([saccver.split('~~')[0]])
+            (qaccver, saccver, length, slen, pident) = b.split("\t")
+            if (
+                float(pident) >= self.pid
+                and 100 * (float(length) / float(slen)) >= self.cov
+            ):
+                self.full_matches.update([saccver.split("~~")[0]])
             else:
-                self.partial_matches.update([saccver.split('~~')[0]])
+                self.partial_matches.update([saccver.split("~~")[0]])
 
-    def tab_report(self, header=True, sep=','):
+    def tab_report(self, header=True, sep=","):
         if header:
             print(sep.join(self.OUTPUT_HEADER))
         out = []
@@ -76,14 +71,14 @@ class Typing:
         print(sep.join(out))
 
     def csv_report(self, header=True):
-        self.tab_report(sep=',', header=header)
+        self.tab_report(sep=",", header=header)
 
     def tsv_report(self, header=True):
-        self.tab_report(sep='\t', header=header)
+        self.tab_report(sep="\t", header=header)
 
 
 class Serotype(Typing):
-    '''
+    """
     Serotyping of Listeria monocytogenes samples is performed throught the
     presence abscense of five genes.
 
@@ -122,92 +117,99 @@ class Serotype(Typing):
         Serogroup 4b, 4d, 4e*
     else:
         Nontypable
-    '''
+    """
 
-    BLAST_OUTFMT = '6 qaccver saccver length slen pident'
-    OUTPUT_HEADER = ['ID', 'SEROTYPE',
-                     'Prs', 'lmo0737',
-                     'lmo1118', 'ORF2110',
-                     'ORF2819', 'COMMENT',
-                     'DB_VERSION']
+    BLAST_OUTFMT = "6 qaccver saccver length slen pident"
+    OUTPUT_HEADER = [
+        "ID",
+        "SEROTYPE",
+        "Prs",
+        "lmo0737",
+        "lmo1118",
+        "ORF2110",
+        "ORF2819",
+        "COMMENT",
+        "DB_VERSION",
+    ]
 
-    def __init__(self, blast_run, db,
-                 cov=100,
-                 pid=98):
-                super().__init__(blast_run=blast_run,
-                                 db=db,
-                                 cov=cov,
-                                 pid=pid)
+    def __init__(self, blast_run, db, cov=100, pid=98):
+        super().__init__(blast_run=blast_run, db=db, cov=cov, pid=pid)
 
     def _blast_parse(self):
         self.full_matches = set()
         self.partial_matches = set()
-        blast_matches = self.blast_res.stdout.strip().split('\n')
+        blast_matches = self.blast_res.stdout.strip().split("\n")
         for b in blast_matches:
-            (qaccver,
-             saccver,
-             length,
-             slen,
-             pident) = b.split('\t')
+            (qaccver, saccver, length, slen, pident) = b.split("\t")
             obs_pid = float(pident)
-            obs_cov = 100 * (float(length)/float(slen))
+            obs_cov = 100 * (float(length) / float(slen))
             if obs_pid >= self.pid and obs_cov >= self.cov:
-                self.full_matches.update([saccver.split('~~')[0].upper()])
+                self.full_matches.update([saccver.split("~~")[0].upper()])
             else:
-                self.partial_matches.update([saccver.split('~~')[0].upper()])
+                self.partial_matches.update([saccver.split("~~")[0].upper()])
 
     def generate_type(self, query):
-        report = {'prs': None,
-                  'lmo0737': None,
-                  'lmo1118': None,
-                  'orf2110': None,
-                  'orf2819': None}
+        report = {
+            "prs": None,
+            "lmo0737": None,
+            "lmo1118": None,
+            "orf2110": None,
+            "orf2819": None,
+        }
         self.blast.add_query(query)
         self._blast_run()
         self._blast_parse()
         for gene in report:
             if gene.upper() in self.full_matches:
-                report[gene] = 'FULL MATCH'
+                report[gene] = "FULL MATCH"
             elif gene.upper() in self.partial_matches:
-                report[gene] = 'PARTIAL MATCH'
+                report[gene] = "PARTIAL MATCH"
             else:
-                report[gene] = 'NOT FOUND'
-        if 'PRS' not in self.full_matches:
-            report['serotype'] = 'Nontypable'
-            report['comment'] = 'No Prs found'
-        elif 'LMO0737' in self.full_matches and\
-             'ORF2819' not in self.full_matches and\
-             'ORF2110' not in self.full_matches:
-            if 'LMO1118' in self.full_matches:
-                report['serotype'] = '1/2c, 3c'
-                report['comment'] = None
+                report[gene] = "NOT FOUND"
+        if "PRS" not in self.full_matches:
+            report["serotype"] = "Nontypable"
+            report["comment"] = "No Prs found"
+        elif (
+            "LMO0737" in self.full_matches
+            and "ORF2819" not in self.full_matches
+            and "ORF2110" not in self.full_matches
+        ):
+            if "LMO1118" in self.full_matches:
+                report["serotype"] = "1/2c, 3c"
+                report["comment"] = None
             else:
-                report['serotype'] = '1/2a, 3a'
-                report['comment'] = None
-        elif 'ORF2819' in self.full_matches and\
-             'LMO0737' not in self.full_matches and\
-             'LMO1118' not in self.full_matches:
-            if 'ORF2110' in self.full_matches:
-                report['serotype'] = '4b, 4d, 4e'
-                report['comment'] = None
+                report["serotype"] = "1/2a, 3a"
+                report["comment"] = None
+        elif (
+            "ORF2819" in self.full_matches
+            and "LMO0737" not in self.full_matches
+            and "LMO1118" not in self.full_matches
+        ):
+            if "ORF2110" in self.full_matches:
+                report["serotype"] = "4b, 4d, 4e"
+                report["comment"] = None
             else:
-                report['serotype'] = "1/2b, 3b, 7"
-                report['comment'] = None
-        elif 'LMO0737' in self.full_matches and\
-             'ORF2819' in self.full_matches and\
-             'ORF2110' in self.full_matches:
-            report['serotype'] = '4b, 4d, 4e*'
-            report['comment'] = 'Unusual 4b with lmo0737'
+                report["serotype"] = "1/2b, 3b, 7"
+                report["comment"] = None
+        elif (
+            "LMO0737" in self.full_matches
+            and "ORF2819" in self.full_matches
+            and "ORF2110" in self.full_matches
+        ):
+            report["serotype"] = "4b, 4d, 4e*"
+            report["comment"] = "Unusual 4b with lmo0737"
         else:
-            report['serotype'] = 'Nontypable'
-            report['comment'] = 'No combination of fully matched genes'\
-                                ' resulted in a known serotype.'
-        report['id'] = query
-        report['db_version'] = self.db_version()
+            report["serotype"] = "Nontypable"
+            report["comment"] = (
+                "No combination of fully matched genes" " resulted in a known serotype."
+            )
+        report["id"] = query
+        report["db_version"] = self.db_version()
         self.report = report
         logging.debug(report)
 
-#delete BT
+
+# delete BT
 """
 class BinaryType(Typing):
     '''
@@ -375,27 +377,31 @@ class BinaryType(Typing):
         logging.debug(self.report)
 """
 
+
 class SerotypeDB:
-    def __init__(self, path_db,
-                 db_type,
-                 db_name='lissero',
-                 title='Listeria Serotyping BLAST DB',
-                 makeblastdb_path=None,
-                 force=False):
+    def __init__(
+        self,
+        path_db,
+        db_type,
+        db_name="lissero",
+        title="Listeria Serotyping BLAST DB",
+        makeblastdb_path=None,
+        force=False,
+    ):
         self.title = title
         self.path_db = os.path.realpath(path_db)
         self.db_name = os.path.join(self.path_db, db_name)
         self.mkdb = MakeBlastDB(makeblastdb_path=makeblastdb_path)
-        if db_type == 'serotype':
-            self.infile = pkg_resources.resource_filename('lissero', SEROTYPE_FASTA)
+        if db_type == "serotype":
+            self.infile = pkg_resources.resource_filename("lissero", SEROTYPE_FASTA)
         # NO binary Type
-        #elif db_type == 'binary_type':
-            #self.infile = pkg_resources.resource_filename('lissero', BINARYTYPE_FASTA)
+        # elif db_type == 'binary_type':
+        # self.infile = pkg_resources.resource_filename('lissero', BINARYTYPE_FASTA)
         else:
-            logging.critical(f'I don\'t understand db_type = {db_type}')
+            logging.critical(f"I don't understand db_type = {db_type}")
             raise IOError
         self.force = force
-        self.log_file = os.path.join(self.path_db, db_name+'_db.json')
+        self.log_file = os.path.join(self.path_db, db_name + "_db.json")
         self.db_log = {}
 
     def check_db(self):
@@ -403,15 +409,17 @@ class SerotypeDB:
             logging.info("Will rebuild database if one exists!")
             self._make_db()
             return
-        elif os.path.exists(self.db_name+'.nhr'):
+        elif os.path.exists(self.db_name + ".nhr"):
             self._load_log()
             logging.info(f"DB was created on {self.db_log['date_created']}")
             logging.info("DB is ready for use.")
         elif self.infile is None:
-            logging.critical("There is no database, and I don't"
-                             " have enough information to create one."
-                             " Please add an input file for the DB"
-                             " and run again.")
+            logging.critical(
+                "There is no database, and I don't"
+                " have enough information to create one."
+                " Please add an input file for the DB"
+                " and run again."
+            )
             raise IOError
         else:
             logging.warning(f"Did not find a DB at {self.path_db}")
@@ -422,11 +430,11 @@ class SerotypeDB:
         return self.db_name
 
     def version(self):
-        return self.db_log['date_created']
+        return self.db_log["date_created"]
 
     def _load_log(self):
         if os.path.exists(self.log_file):
-            fh = open(self.log_file, 'rt')
+            fh = open(self.log_file, "rt")
             self.db_log = json.load(fh)
             logging.info("Successfully loaded DB log!")
             fh.close()
@@ -434,32 +442,32 @@ class SerotypeDB:
             logging.warning("Did find a log file at {self.path_db}")
 
     def _save_log(self):
-        fh = open(self.log_file, 'wt')
+        fh = open(self.log_file, "wt")
         json.dump(self.db_log, fh, indent=4)
         logging.info("Successfully saved log!")
 
     def _hash_file(self, filename):
-        f = open(filename, 'rb')
+        f = open(filename, "rb")
         file_content = f.read()
         h = hashlib.sha256(file_content).hexdigest()
         f.close()
         return h
 
     def _make_db(self):
-        self.db_log['date_created'] = datetime.datetime.today().isoformat()
+        self.db_log["date_created"] = datetime.datetime.today().isoformat()
         mkdb = self.mkdb
-        mkdb.add_option('-dbtype', 'nucl')
-        mkdb.add_option('-in', self.infile)
-        mkdb.add_option('-parse_seqids')
-        mkdb.add_option('-input_type', 'fasta')
-        mkdb.add_option('-out', self.db_name)
-        mkdb.add_option('-title', shlex.quote(self.title))
+        mkdb.add_option("-dbtype", "nucl")
+        mkdb.add_option("-in", self.infile)
+        mkdb.add_option("-parse_seqids")
+        mkdb.add_option("-input_type", "fasta")
+        mkdb.add_option("-out", self.db_name)
+        mkdb.add_option("-title", shlex.quote(self.title))
         mkdb.run()
-        self.db_log['cmd'] = str(mkdb)
-        self.db_log['creator'] = getpass.getuser()
-        self.db_log['infile'] = {}
-        self.db_log['infile']['path'] = self.infile
-        self.db_log['infile']['sha256'] = self._hash_file(self.infile)
-        self.db_log['title'] = self.title
+        self.db_log["cmd"] = str(mkdb)
+        self.db_log["creator"] = getpass.getuser()
+        self.db_log["infile"] = {}
+        self.db_log["infile"]["path"] = self.infile
+        self.db_log["infile"]["sha256"] = self._hash_file(self.infile)
+        self.db_log["title"] = self.title
         self._save_log()
         logging.info(f"Successfully created {self.db_name}")
