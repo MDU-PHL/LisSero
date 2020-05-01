90d7cf52a2944fb0462184bb17526d0b0a983dad
Anders Goncalves da Silva <andersgs@gmail.com>
2020-04-30 17:22:59 -0700

    Removed binary typing refs

--- lissero/run_lissero.py
+++ lissero/run_lissero.py
@@ -12,19 +12,13 @@ from lissero.scripts.Serotype import SerotypeDB
 
 
 @click.command()
-@click.option("-s", "--serotype_db", default=None,
-              envvar='LISSERO_DB')
-#@click.option("-b", "--binarytype_db", default=None,envvar='LISBT_DB')
-#@click.option("--bt_min_id", default=90)
-#@click.option("--bt_min_cov", default=95)
+@click.option("-s", "--serotype_db", default=None,  envvar="LISSERO_DB")
 @click.option("--sg_min_id", default=95)
 @click.option("--sg_min_cov", default=95)
 @click.option("--debug", is_flag=True)
-@click.argument("fasta", nargs=-1)
-def run_lissero(serotype_db, 
-                #binarytype_db,
+@click.argument("fasta", nargs=-1, type=click.Path(), required=True)
+def run_lissero(serotype_db,
                 sg_min_id, sg_min_cov,
-                #bt_min_id, bt_min_cov,
                 debug,
                 fasta):
 
@@ -32,17 +26,10 @@ def run_lissero(serotype_db,
     In silico serogroup prediction for L. monocytogenes.
     Alleles: lmo1118, lmo0737, ORF2819, ORF2110, Prs
 
-    In silico binary type prediction for L. monocytogenes.
-    Alleles: 1, 2, 4, 8, 16, 32, 64, 128
-
     References:
 
     * Doumith et al. Differentiation of the major Listeria monocytogenes
         serovars by multiplex PCR. J Clin Microbiol, 2004; 42:8; 3819-22
-
-    * Huang et al. Binary typing of *Listeria monocytogenes* isolates from
-        patients and food through multiplex PCR and reverse line hybridisation.
-        SA, Australia, 2007; 136–137.
 
     '''
     if debug:
@@ -51,25 +38,15 @@ def run_lissero(serotype_db,
         log_level = logging.INFO
     logging.basicConfig(level=log_level)
     path_serodb = os.path.realpath(serotype_db)
-    #path_btdb = os.path.realpath(binarytype_db)
     sero_db = SerotypeDB(path_db=path_serodb,
                          db_type='serotype')
     sero_db.check_db()
-    """
-    bt_db = SerotypeDB(path_db=path_btdb,
-                       db_type='binary_type',
-                       db_name='lisbt')
-    bt_db.check_db()
-    """
     blast = Blast()
     samples = Samples(fasta,
                       blast=blast,
                       sero_db=sero_db,
                       sg_min_id=sg_min_id,
                       sg_min_cov=sg_min_cov)
-                      #bt_db=bt_db,
-                      #bt_min_id=bt_min_id,
-                      #bt_min_cov=bt_min_cov)
     samples.run_typing()
     samples.simple_report()
 
