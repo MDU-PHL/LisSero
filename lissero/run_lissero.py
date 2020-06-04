b76a47b2cf5ed5d20159a31073834d7ff60f15ce
Anders Goncalves da Silva <andersgs@gmail.com>
2020-06-04 11:53:57 -0700

    Implement better logging and input file checking
    * Allow for logging to a file or to stderr using loguru
    * Implement FASTA checking
    * Implement error when no file is provided
    * Implement error when provided file(s) don't exist

--- lissero/run_lissero.py
+++ lissero/run_lissero.py
@@ -5,16 +5,51 @@ import sys
 import logging
 import os
 
+import click
+import loguru
+from Bio import SeqIO
+
 from lissero.scripts.Sample import Samples
 from lissero.scripts.Blast import Blast
 from lissero.scripts.Serotype import SerotypeDB
 from .__init__ import __version__ as version
+
+logger = loguru.logger
+
 
 def print_version(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
     click.echo(f"LisSero {version}")
     ctx.exit()
+
+
+def is_fasta(filename):
+    """
+    There are no real FASTA validators out there.
+    This is the best I could come up with. If the
+    file is empty or does not contain any FASTA records
+    the parser will return an empty generator which will
+    return false in `any(gen)`.
+    But, there is another case where the file starts with
+    a `>`, and thus the generator works, but it returns an
+    empty record, thus the `and len(next(gen)) > 0.
+
+    Args:
+        filename: FASTA input name
+
+    Returns:
+        boolean: true if it looks like a FASTA false otherwise.
+    """
+    gen = SeqIO.parse(filename, "fasta")
+    try:
+        rec = next(gen)
+        return len(rec) > 0
+    except StopIteration:
+        return False
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
 
 @click.command()
 @click.option("-s", "--serotype_db", default=None, envvar="LISSERO_DB")
@@ -31,13 +66,12 @@ def print_version(ctx, param, value):
     show_default=True,
 )
 @click.option("--debug", is_flag=True)
-@click.argument("fasta", nargs=-1, type=click.Path(), required=True)
-#fix Version Issue #10
-@click.option("--version", is_flag=True, callback=print_version, \
+@click.option("--logfile", help="Save log to a file instead of printing to stderr", default="")
+@click.argument("fasta", nargs=-1, type=click.Path(exists=True), required=True)
+# fix Version Issue #10
+@click.option("--version", is_flag=True, callback=print_version,
               expose_value=False, is_eager=True, help="Show Version Information")
-
-def run_lissero(serotype_db, min_id, min_cov, debug, fasta):
-
+def run_lissero(serotype_db, min_id, min_cov, debug, logfile, fasta):
     """
     In silico serogroup prediction for L. monocytogenes.
     Alleles: lmo1118, lmo0737, ORF2819, ORF2110, Prs
@@ -48,18 +82,32 @@ def run_lissero(serotype_db, min_id, min_cov, debug, fasta):
         serovars by multiplex PCR. J Clin Microbiol, 2004; 42:8; 3819-22
 
     """
+
     if debug:
         log_level = logging.DEBUG
     else:
         log_level = logging.INFO
-    logging.basicConfig(filename='./lissero.log', level=log_level)
-    #fix issue #11 and #12
+
+    if logfile != "":
+        logger.remove()
+        logger.add(logfile, level=log_level)
+    else:
+        logger.remove()
+        logger.add(sys.stderr, level=log_level)
+
+    all_fasta = all([is_fasta(fna) for fna in fasta])
+    if not all_fasta:
+        logger.error("One or more input files do not appear to be "
+                     "valid FASTA.")
+        sys.exit(1)
+
+    # fix issue #11 and #12
     path_serodb = ""
     try:
         path_serodb = os.path.realpath(serotype_db)
-    except:
-        logging.error(f"Please provide a correct serotype db path or set correct PATH for LISSERO_DB")
-        sys.exit()
+    except TypeError as e:
+        logger.error(f"Please provide a correct serotype db path or set correct PATH for LISSERO_DB")
+        sys.exit(1)
     sero_db = SerotypeDB(path_db=path_serodb, db_type="serotype")
     sero_db.check_db()
     blast = Blast()
