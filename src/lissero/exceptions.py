7793208d4fef977ff2f741ac19c89e0eed909896
Anders Goncalves da Silva <andersgs@gmail.com>
2023-02-25 19:42:20 -0800

    Disable lint warning for unnecessary pass
    I find the use of pass here a good delimiter and aids in reading

--- src/lissero/exceptions.py
+++ src/lissero/exceptions.py
@@ -2,29 +2,38 @@
 Custom exceptions for lissero
 """
 
+
+# pylint: disable=unnecessary-pass
 class InvalidFASTAFormat(Exception):
     """
     Exception raised when a FASTA file is invalid because it does not contain
     a first line starting with '>'
     """
+
     pass
+
 
 class InvalidFASTADuplicateIdentifiers(Exception):
     """
     Exception raised when a FASTA file is invalid because it contains duplicate
     identifiers
     """
+
     pass
+
 
 class InvalidFASTAEmptyFile(Exception):
     """
     Exception raised when a FASTA file is invalid because it is empty
     """
+
     pass
 
-class InvalidFASTAInvalidCharacters(Exception): 
+
+class InvalidFASTAInvalidCharacters(Exception):
     """
     Exception raised when a FASTA file is invalid because it contains invalid
     characters (not A, C, G, T, N, a, c, g, t, n)
     """
+
     pass
